899dd2e43aa9c05a1b1ce2fb1baba0507608e83f
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-10-28 16:44:25 +0100

    WIP testing caliber distances

--- labels_manager/tools/caliber/distances.py
+++ labels_manager/tools/caliber/distances.py
@@ -23,19 +23,27 @@ def centroid_array(arr, labels):
 
 def centroid(im, labels, return_mm3=True):
     """
-    Centroid (center of mass, baricenter) of a list of labels.
+    Centroid (center of mass, barycenter) of a list of labels.
     :param im:
     :param labels: list of labels, e.g. [3] or [2, 3, 45]
     :param return_mm3: if true the answer is in mm if false in voxel indexes.
     :return: list of centroids, one for each label in the input order.
     """
     centers_of_mass = centroid_array(im.get_data(), labels)
-
+    ans = []
     if return_mm3:
-        centers_of_mass = [im.affine[:3, :3].dot(cm.astype(np.float64)) for cm in centers_of_mass]
+        for cm in centers_of_mass:
+            if isinstance(cm, np.ndarray):
+                ans += [im.affine[:3, :3].dot(cm.astype(np.float64))]
+            else:
+                ans += [cm]
     else:
-        centers_of_mass = [np.round(cm).astype(np.uint64) for cm in centers_of_mass]
-    return centers_of_mass
+        for cm in centers_of_mass:
+            if isinstance(cm, np.ndarray):  # else it is np.nan.
+                ans += [np.round(cm).astype(np.uint64)]
+            else:
+                ans += [cm]
+    return ans
 
 # --- distances
 
@@ -51,9 +59,12 @@ def dice_score(im_segm1, im_segm2, labels_list, labels_names, verbose=0):
     def dice_score_l(lab):
         place1 = im_segm1.get_data() == lab  # slow but readable, can be refactored later.
         place2 = im_segm2.get_data() == lab
-        assert isinstance(place1, np.ndarray)
-        assert isinstance(place2, np.ndarray)
-        return 2 * np.count_nonzero(place1 * place2) / float(np.count_nonzero(place1) + np.count_nonzero(place2))
+        non_zero_place1 = np.count_nonzero(place1)
+        non_zero_place2 = np.count_nonzero(place2)
+        if non_zero_place1 + non_zero_place2 == 0:
+            return np.nan
+        else:
+            return 2 * np.count_nonzero(place1 * place2) / float(non_zero_place1 + non_zero_place2)
 
     scores = []
     for l in labels_list:
@@ -73,10 +84,10 @@ def global_dice_score(im_segm1, im_segm2, labels_list):
     :param labels_list:
     :return:
     """
-    sum_intersections = [np.count_nonzero(im_segm1.get_data() == l * im_segm2.get_data() == l)
-                         for l in labels_list]
+    sum_intersections = np.sum([np.count_nonzero((im_segm1.get_data() == l) * (im_segm2.get_data() == l))
+                         for l in labels_list])
 
-    return 2 * sum_intersections / (np.count_nonzero(im_segm1.get_data()) + np.count_nonzero(im_segm2.get_data()))
+    return 2 * sum_intersections / float(np.count_nonzero(im_segm1.get_data()) + np.count_nonzero(im_segm2.get_data()))
 
 
 def global_outline_error(im_segm1, im_segm2, labels_list):
@@ -93,80 +104,7 @@ def global_outline_error(im_segm1, im_segm2, labels_list):
     return num_voxels_diff / (.5 * (num_voxels_1 + num_voxels_2))
 
 
-def s_dispersion(im_segm1, im_segm2, labels_list, labels_names, return_mm3=True, verbose=0):
-    # definition of s_dispersion is not the conventional definition of precision
-
-    # def dispersion_l(lab, verbose=verbose):
-    #     c1 = centroid_array(im_segm1.get_data(), labels=[lab,])[0]
-    #     c2 = centroid_array(im_segm2.get_data(), labels=[lab,])[0]
-    #     if return_mm3:
-    #         c1 = im_segm1.affine[:3, :3].dot(c1.astype(np.float64))
-    #         c2 = im_segm1.affine[:3, :3].dot(c2.astype(np.float64))
-    #     d = np.sqrt( sum((c1 - c2)**2) )
-    #     if verbose > 0:
-    #         print('Dispersion, label {0} : {1}'.format(l, d))
-    #     return d
-    #
-    # np.testing.assert_array_almost_equal(im_segm1.affine, im_segm2.affine)
-    # return pa.Series(np.array([dispersion_l(l) for l in labels_list]), index=labels_names)
-
-    cs1 = centroid_array(im_segm1.get_data(), labels=labels_list)
-    cs2 = centroid_array(im_segm2.get_data(), labels=labels_list)
-    if return_mm3:
-        cs1 = [im_segm1.affine[:3, :3].dot(c.astype(np.float64)) for c in cs1]
-        cs2 = [im_segm1.affine[:3, :3].dot(c.astype(np.float64)) for c in cs2]
-
-    np.testing.assert_array_almost_equal(im_segm1.affine, im_segm2.affine)
-    return pa.Series(np.array([np.sqrt( sum((c1 - c2)**2) ) for c1, c2 in zip(cs1, cs2)]), index=labels_names)
-
-
-def s_precision(im_segm1, im_segm2, pfo_intermediate_files, labels_list, labels_names, verbose=0):
-    # deprecated as too imprecise :-) ! -  definition of s_precision is not the conventional definition of precision
-    print_and_run('mkdir -p {}'.format(pfo_intermediate_files))
-    precision_per_label = []
-
-    for l in labels_list:
-        im_segm_single1 = set_new_data(im_segm1, keep_only_one_label(im_segm1.get_data(), l))
-        im_segm_single2 = set_new_data(im_segm2, keep_only_one_label(im_segm2.get_data(), l))
-
-        pfi_im_segm_single1 = os.path.join(pfo_intermediate_files, 'im_segm_single_lab{}_1.nii.gz'.format(l))
-        pfi_im_segm_single2 = os.path.join(pfo_intermediate_files, 'im_segm_single_lab{}_2.nii.gz'.format(l))
-
-        nib.save(im_segm_single1, pfi_im_segm_single1)
-        nib.save(im_segm_single2, pfi_im_segm_single2)
-
-        pfi_warp_1_2 = os.path.join(pfo_intermediate_files, 'warped_single_lab{}_1_on_2.nii.gz'.format(l))
-        pfi_warp_2_1 = os.path.join(pfo_intermediate_files, 'warped_single_lab{}_2_on_1.nii.gz'.format(l))
-        pfi_aff_1_2 = os.path.join(pfo_intermediate_files, 'aff_single_lab{}_1_on_2.txt'.format(l))
-        pfi_aff_2_1 = os.path.join(pfo_intermediate_files, 'warped_single_lab{}_2_on_1.txt'.format(l))
-
-        cmd1_2 = 'reg_aladin -ref {0} -flo {1} -res {2} -aff {3} -interp 0 -speeeeed'.format(pfi_im_segm_single1,
-                                                                                             pfi_im_segm_single2,
-                                                                                             pfi_warp_1_2,
-                                                                                             pfi_aff_1_2)
-        cmd2_1 = 'reg_aladin -ref {0} -flo {1} -res {2} -aff {3} -interp 0 -speeeeed'.format(pfi_im_segm_single2,
-                                                                                             pfi_im_segm_single1,
-                                                                                             pfi_warp_2_1,
-                                                                                             pfi_aff_2_1)
-        print_and_run(cmd1_2)
-        print_and_run(cmd2_1)
-
-        t_1_2 = np.loadtxt(pfi_aff_1_2)
-        t_2_1 = np.loadtxt(pfi_aff_2_1)
-
-        d = 1 / float(np.max([np.abs(np.linalg.det(t_1_2)) ** (-1), np.abs(np.linalg.det(t_2_1)) ** (-1)]))
-        precision_per_label.append(d)
-        if verbose > 0:
-            print('------------------------------------')
-            print('Precision, label {0} : {1}'.format(l, d))
-            print('------------------------------------')
-    pa_series = pa.Series(np.array(precision_per_label), index=labels_names)
-    pa_series.to_pickle(os.path.join(pfo_intermediate_files, 'final_precision.pickle'))
-
-    return pa_series
-
-
-def covariance_matrices(im, labels, return_mm3=True, subtract_mean=True):
+def covariance_matrices(im, labels, return_mm3=True):
     """
     Considers the label as a point distribution in the space, and returns the covariance matrix of the points
     distributions.
@@ -178,9 +116,10 @@ def covariance_matrices(im, labels, return_mm3=True, subtract_mean=True):
     cov_matrices = [np.zeros([3, 3])] * len(labels)
     for l_id, l in enumerate(labels):
         coords = np.where(im.get_data() == l)  # returns [X_vector, Y_vector, Z_vector]
-        if subtract_mean:
-            coords = [coords[j] - np.mean(coords[j]) for j in range(3)]
-        cov_matrices[l_id] = np.cov(coords)
+        if np.count_nonzero(coords) > 0:
+            cov_matrices[l_id] = np.cov(coords)
+        else:
+            cov_matrices[l_id] = np.nan * np.ones([3, 3])
     if return_mm3:
         cov_matrices = [im.affine[:3, :3].dot(cm.astype(np.float64)) for cm in cov_matrices]
 
@@ -202,8 +141,10 @@ def covariance_distance(im_segm1, im_segm2, labels_list, labels_names, return_mm
         :param m2: matrix
         :return: 1 - (np.trace(m1.dot(m2))) / (np.linalg.norm(m1) + np.linalg.norm(m2))
         """
-        return 1 - (np.trace(m1.dot(m2)) / (np.linalg.norm(m1, ord='fro') + np.linalg.norm(m2, ord='fro')))
-
+        if np.nan not in m1 and np.nan not in m2:
+            return 1 - (np.trace(m1.dot(m2)) / (np.linalg.norm(m1, ord='fro') * np.linalg.norm(m2, ord='fro')))
+        else:
+            return np.nan
     cvs1 = covariance_matrices(im_segm1, labels=labels_list, return_mm3=return_mm3)
     cvs2 = covariance_matrices(im_segm2, labels=labels_list, return_mm3=return_mm3)
 
@@ -313,3 +254,78 @@ def mahalanobis_distance(im, im_mask=None, trim=False):
         if trim:
             new_data = new_data * im_mask.get_data().astype(np.bool)
         return set_new_data(im, new_data)
+
+
+
+
+def s_dispersion(im_segm1, im_segm2, labels_list, labels_names, return_mm3=True, verbose=0):
+    # definition of s_dispersion is not the conventional definition of precision
+
+    # def dispersion_l(lab, verbose=verbose):
+    #     c1 = centroid_array(im_segm1.get_data(), labels=[lab,])[0]
+    #     c2 = centroid_array(im_segm2.get_data(), labels=[lab,])[0]
+    #     if return_mm3:
+    #         c1 = im_segm1.affine[:3, :3].dot(c1.astype(np.float64))
+    #         c2 = im_segm1.affine[:3, :3].dot(c2.astype(np.float64))
+    #     d = np.sqrt( sum((c1 - c2)**2) )
+    #     if verbose > 0:
+    #         print('Dispersion, label {0} : {1}'.format(l, d))
+    #     return d
+    #
+    # np.testing.assert_array_almost_equal(im_segm1.affine, im_segm2.affine)
+    # return pa.Series(np.array([dispersion_l(l) for l in labels_list]), index=labels_names)
+
+    cs1 = centroid_array(im_segm1.get_data(), labels=labels_list)
+    cs2 = centroid_array(im_segm2.get_data(), labels=labels_list)
+    if return_mm3:
+        cs1 = [im_segm1.affine[:3, :3].dot(c.astype(np.float64)) for c in cs1]
+        cs2 = [im_segm1.affine[:3, :3].dot(c.astype(np.float64)) for c in cs2]
+
+    np.testing.assert_array_almost_equal(im_segm1.affine, im_segm2.affine)
+    return pa.Series(np.array([np.sqrt( sum((c1 - c2)**2) ) for c1, c2 in zip(cs1, cs2)]), index=labels_names)
+
+
+def s_precision(im_segm1, im_segm2, pfo_intermediate_files, labels_list, labels_names, verbose=0):
+    # deprecated as too imprecise :-) ! -  definition of s_precision is not the conventional definition of precision
+    print_and_run('mkdir -p {}'.format(pfo_intermediate_files))
+    precision_per_label = []
+
+    for l in labels_list:
+        im_segm_single1 = set_new_data(im_segm1, keep_only_one_label(im_segm1.get_data(), l))
+        im_segm_single2 = set_new_data(im_segm2, keep_only_one_label(im_segm2.get_data(), l))
+
+        pfi_im_segm_single1 = os.path.join(pfo_intermediate_files, 'im_segm_single_lab{}_1.nii.gz'.format(l))
+        pfi_im_segm_single2 = os.path.join(pfo_intermediate_files, 'im_segm_single_lab{}_2.nii.gz'.format(l))
+
+        nib.save(im_segm_single1, pfi_im_segm_single1)
+        nib.save(im_segm_single2, pfi_im_segm_single2)
+
+        pfi_warp_1_2 = os.path.join(pfo_intermediate_files, 'warped_single_lab{}_1_on_2.nii.gz'.format(l))
+        pfi_warp_2_1 = os.path.join(pfo_intermediate_files, 'warped_single_lab{}_2_on_1.nii.gz'.format(l))
+        pfi_aff_1_2 = os.path.join(pfo_intermediate_files, 'aff_single_lab{}_1_on_2.txt'.format(l))
+        pfi_aff_2_1 = os.path.join(pfo_intermediate_files, 'warped_single_lab{}_2_on_1.txt'.format(l))
+
+        cmd1_2 = 'reg_aladin -ref {0} -flo {1} -res {2} -aff {3} -interp 0 -speeeeed'.format(pfi_im_segm_single1,
+                                                                                             pfi_im_segm_single2,
+                                                                                             pfi_warp_1_2,
+                                                                                             pfi_aff_1_2)
+        cmd2_1 = 'reg_aladin -ref {0} -flo {1} -res {2} -aff {3} -interp 0 -speeeeed'.format(pfi_im_segm_single2,
+                                                                                             pfi_im_segm_single1,
+                                                                                             pfi_warp_2_1,
+                                                                                             pfi_aff_2_1)
+        print_and_run(cmd1_2)
+        print_and_run(cmd2_1)
+
+        t_1_2 = np.loadtxt(pfi_aff_1_2)
+        t_2_1 = np.loadtxt(pfi_aff_2_1)
+
+        d = 1 / float(np.max([np.abs(np.linalg.det(t_1_2)) ** (-1), np.abs(np.linalg.det(t_2_1)) ** (-1)]))
+        precision_per_label.append(d)
+        if verbose > 0:
+            print('------------------------------------')
+            print('Precision, label {0} : {1}'.format(l, d))
+            print('------------------------------------')
+    pa_series = pa.Series(np.array(precision_per_label), index=labels_names)
+    pa_series.to_pickle(os.path.join(pfo_intermediate_files, 'final_precision.pickle'))
+
+    return pa_series
