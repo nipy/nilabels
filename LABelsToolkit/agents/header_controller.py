BUG apply small rotation. ENH now small rotation respect to origin and centre of image fov

LABelsToolkit/agents/header_controller.py
@@ -50,24 +50,41 @@ class LABelsToolkitHeaderController(object):
                                               multiplication_side=multiplication_side)
         nib.save(new_im, pfi_out)
 
-    def apply_small_rotation(self, filename_in, filename_out, angle=np.pi/6, principal_axis='pitch'):
-        # Warning: this works only for hisotropic spaces. If the space is not isotropic, need to
-        # do make it isotropic first.
+    def apply_small_rotation(self, filename_in, filename_out, angle=np.pi/6, principal_axis='pitch',
+                             respect_to_centre=True):
+
         if isinstance(angle, list):
             assert isinstance(principal_axis, list)
             assert len(principal_axis) == len(angle)
-            new_aff = np.identity(4)
+            rot = np.identity(4)
             for pa, an in zip(principal_axis, angle):
                 aff = get_small_orthogonal_rotation(theta=an, principal_axis=pa)
-                new_aff = new_aff.dot(aff)
+                rot = rot.dot(aff)
         else:
-            new_aff = get_small_orthogonal_rotation(theta=angle, principal_axis=principal_axis)
-
+            rot = get_small_orthogonal_rotation(theta=angle, principal_axis=principal_axis)
+        
         pfi_in, pfi_out = get_pfi_in_pfi_out(filename_in, filename_out, self.pfo_in, self.pfo_out)
         im = nib.load(pfi_in)
 
+        if respect_to_centre:
+            fov_centre = im.affine.dot(np.array(list(np.array(im.shape) / float(2)) + [1]))
+
+            transl = np.eye(4)
+            transl[:3, 3] = fov_centre[:3]
+
+            transl_inv = np.eye(4)
+            transl_inv[:3, 3] = -1 * fov_centre[:3]
+
+            rt = transl.dot(rot.dot(transl_inv))
+
+            new_aff = rt.dot(im.affine)
+        else:
+            new_aff = im.get_affine()[:]
+            new_aff[:3, :3] = rot[:3, :3].dot(new_aff[:3, :3])
+
         new_im = modify_affine_transformation(im_input=im, new_aff=new_aff, q_form=True, s_form=True,
-                                              multiplication_side='right')
+                                              multiplication_side='replace')
+
         nib.save(new_im, pfi_out)
 
     def modify_translational_part(self, filename_in, filename_out, new_translation):

LABelsToolkit/tools/aux_methods/utils_rotations.py
@@ -4,9 +4,9 @@ import numpy as np
 def get_small_orthogonal_rotation(theta, principal_axis='pitch'):
     if principal_axis == 'yaw':
         rot = np.array([[np.cos(theta), -np.sin(theta), 0, 0],
-                        [np.sin(theta), np.cos(theta), 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 0, 0, 1]])
+                        [np.sin(theta), np.cos(theta),  0, 0],
+                        [0,             0,              1, 0],
+                        [0,             0,              0, 1]])
     elif principal_axis == 'pitch':
         rot = np.array([[1,            0,           0,       0],
                         [0,  np.cos(theta),  -np.sin(theta), 0],
@@ -23,7 +23,7 @@ def get_small_orthogonal_rotation(theta, principal_axis='pitch'):
     return rot  # to be multiplied on the right side as im_input.get_affine().dot(rot)
 
 
-def get_roto_translation_matrix(theta, rotation_axis=np.array([1,0,0]),  translation=np.array([0, 0, 0])):
+def get_roto_translation_matrix(theta, rotation_axis=np.array([1, 0, 0]),  translation=np.array([0, 0, 0])):
 
     n = np.linalg.norm(rotation_axis)
     assert not np.abs(n) < 0.001, 'rotation axis too close to zero.'